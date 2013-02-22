9b66f833ec051cc53a97936d8ffbd12981aab40e
lukasz <lukasz.jackowski@itcrowd.pl>
2013-02-22 14:49:30 +0100

    Create CalculatorTest class with beforeTest method

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void beforeTest()
+    {
+        calculator = new Calculator();
+    }
+
+}
